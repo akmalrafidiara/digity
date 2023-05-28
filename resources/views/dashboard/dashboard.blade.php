c0e24a1dafbe071f0f48d70025a8d0425ae5d630
Akmal Rafi Diara Putra <49126626+akmalrafidiara@users.noreply.github.com>
2023-05-28 20:12:48 +0700

    add status widget

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -47,7 +47,48 @@
                     <h1>Dashboard</h1>
                     <p>Hi, Welcome to Digital Entity</p>
                 </div>
-            </div>
+                <div class="status-process">
+                    <div class="card">
+                        <div class="card-title">
+                            <h3>Order Compleate</h3>
+                        </div>
+                        <div class="card-body">
+                            <h1>10<span>/18</span></h1>
+                            <div class="progress">
+                                <div class="progress-bar" style="width: 55%;">55%</div>
+                            </div>
+                        </div>
+                        <a href=""><i class="fa-solid fa-arrow-right"></i> Details</a>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-title">
+                            <h3>Waiting for Revision</h3>
+                        </div>
+                        <div class="card-body">
+                            <h1>2</h1>
+                            <div class="progress">
+                                <div class="progress-bar" style="width: 100%; background-color:#FFEE57 ;">100%</div>
+                            </div>
+                        </div>
+                        <a href=""><i class="fa-solid fa-arrow-right"></i> Details</a>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-title">
+                            <h3>Success</h3>
+                        </div>
+                        <div class="card-body">
+                            <h1>8<span>/10</span></h1>
+                            <div class="progress">
+                                <div class="progress-bar" style="width: 100%; background-color:#73FF9A ;">100%</div>
+                            </div>
+                        </div>
+                        <a href=""><i class="fa-solid fa-arrow-right"></i> Details </a>
+                    </div>
+                </div>
+
+
         </section>
     </div>
 
