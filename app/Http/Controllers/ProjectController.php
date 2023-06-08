be3f8f6abab297fde77b49d89e17df732b803652
fathanbk <fathanbk08@gmail.com>
2023-06-08 22:53:16 +0700

    fixed controller

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Projects extends Controller
+class ProjectController extends Controller
 {
     public function index()
     {
